5d9605d33f1a1077328fadba00872f2c032ec59b
Muzzi Khan <156385618+muzzigit@users.noreply.github.com>
2024-09-03 04:03:12 -0400

    Added project 1

--- src/projects/projectsData.js
+++ src/projects/projectsData.js
@@ -4,17 +4,16 @@ import projectThree from "../assets/project-3.png";
 
 const projects = {
   1: {
-    title: "SoMe Platform",
+    title: "Personal Porrtfolio Website",
     image: projectOne,
     description: (
       <>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        I built this portfolio as a game-inspired app using HTML, CSS, JavaScript, and React to make exploring my skills and projects fun and interactive. The site features dynamic animations, smooth transitions, and a unique navigation experience, turning each section into an exciting journey. It's a creative spin on a classic portfolio, designed to engage and impress potential employers and collaborators.
         </p>
       </>
     ),
-    github: "https://github.com",
+    github: "https://github.com/muzzigit/react-portfolio-v1",
     demo: "https://netlify.com",
   },
   2: {
