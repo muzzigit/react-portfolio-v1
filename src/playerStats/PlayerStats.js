04776306fd2be4d8ec5071f73f6ca42003365635
Muzzi Khan <156385618+muzzigit@users.noreply.github.com>
2024-10-13 02:50:22 -0400

    message

--- src/playerStats/PlayerStats.js
+++ src/playerStats/PlayerStats.js
@@ -4,15 +4,16 @@ import "../styles/playerStats.css";
 const PlayerStats = () => {
   return (
     <div id="playerStats">
-      <h1>MUZZI KHAN LEVEL 72</h1>
+      <h1>MUZZI KHAN LEVEL 75</h1>
       <div id="playerStats__lines">
         <span id="playerStats__lines__thick"></span>
         <span id="playerStats__lines__thin"></span>
       </div>
-      <h2>Software Engineer
+      <h2>Software Developer 🧑🏽‍💻
       </h2>
     </div>
   );
 };
+
 
 export default PlayerStats;
